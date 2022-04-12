25bb2969aa39c2be6191968f54acb942b2a4f7f5
LoicNG6 <loicnguessie@hotmail.com>
2022-04-12 16:26:52 +0200

    LoadingPage + LoginRegistrationPage = done

--- defectueux/App.js
+++
@@ -1,8 +0,0 @@
-import LoadingPage from './src/components/LoadingPage';
-import { Provider as PaperProvider } from 'react-native-paper';
-
-export default function App() {
-  return (
-    <LoadingPage />
-  );
-}
