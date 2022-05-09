f1cefdd044300311a3e8b09960b07575d3563f17
LoicNG6 <loicnguessie@hotmail.com>
2022-05-09 17:33:10 +0200

    registration page : 1/2

--- front/screens/authentication/Register.tsx
+++ front/screens/authentication/Register.tsx
@@ -1,158 +1,242 @@
-import React, { useState, useRef } from "react";
+import React, { useState } from "react";
 import LinearGradient from 'react-native-linear-gradient';
-
 import {
   View,
   Text,
   TextInput,
   StyleSheet,
   Image,
-  Pressable
+  Pressable,
 } from "react-native";
 import MainContainer from "../../components/styles/Container";
-import { Picker } from '@react-native-picker/picker';
-
+import SelectDropdown from 'react-native-select-dropdown'
 
 export default function Registration() {
+  const logoPath = require('../../assets/images/rte-logo.png');
+  // const nextIconPath = require('../../assets/icons/arrow_icon.svg');
+  const [diploma, setDiploma] = useState("none");
 
-  const [selectedValue, setSelectedValue] = useState("java");
+  // Declare all states
+  const [is_etudiant, set_is_etudiant] = useState(false);
+  const [is_lyceen, set_is_lyceen] = useState(false);
+
+  const [school, setSchool] = useState({
+    name: null,
+    address: null,
+    diploma: null,
+    year_of_graduation: null,
+  });
+
+  const graduation_year = ["Choisir l'année", "2022", "2023", "2024", "2025", "2026", "2027"]
+
+  const diploma_types = is_lyceen
+    ? ["Choisir le diplôme", "Baccalauréat"]
+    : is_etudiant ? [
+      "Choisir le diplôme",
+      "Brevet Technique Supérieur (BTS)",
+      "Diplôme universitaire technique (DUT)",
+      "License",
+      "Master",
+      "Doctorat",
+    ] : [];
+
+  // modification of states
+  const onChangeStatus = (name: string) => {
+    if (name == 'lyceen') {
+      set_is_etudiant(false)
+      set_is_lyceen(!is_lyceen);
+    }
+    else {
+      set_is_lyceen(false);
+      set_is_etudiant(!is_etudiant);
+    }
+  }
+
+  const onChangeSchool = (field, value) => {
+    setSchool(prevState => ({
+      ...prevState,
+      [field]: value,
+    }));
+  };
+
+  // just to see school object 
+  if (school.year_of_graduation) console.log(school);
+
   return (
-    <View style={{ flex: 1 }}>
-
+    <View style={{ flex: 1, }}>
       <LinearGradient
         colors={['#CC9644', '#90621C']}
         start={{ x: 0, y: 0 }}
         end={{ x: 1, y: 1 }}
-        style={MainContainer.container}
+        style={[MainContainer.container, { padding: 10, paddingTop: 0 }]}
       >
-        <View style={styles.container}>
-          <Picker
-            selectedValue={selectedValue}
-            style={{ height: 50, width: 150 }}
-            onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}
-          >
-            <Picker.Item label="Java" value="java" />
-            <Picker.Item label="JavaScript" value="js" />
-          </Picker>
+        <View style={[MainContainer.container]}>
+          <View style={[RegisterStyle.main]}>
+
+            <View>
+              <Image source={logoPath} style={MainContainer.logo_in_the_app} />
+            </View>
+
+            <View style={[
+              RegisterStyle.container,
+              {
+                flex: 0.2,
+              }
+            ]}>
+              <View>
+                <Text style={[RegisterStyle.text]}>
+                  Informations relatives à mon établissement
+                </Text>
+              </View>
+              <View>
+                <Text style={[
+                  RegisterStyle.text,
+                  { marginTop: 20, marginBottom: 10 }
+                ]}>
+                  Je suis :
+                </Text>
+              </View>
+              <View style={[RegisterStyle.status_type]}>
+                <Pressable
+                  onPress={() => onChangeStatus('lyceen')}
+                  style={is_lyceen ? RegisterStyle.pressed : RegisterStyle.button}
+                >
+                  <Text style={is_lyceen ? RegisterStyle.pressed_text : RegisterStyle.text}>
+                    Lycéen(ne)
+                  </Text>
+                </Pressable>
+                <Pressable
+                  onPress={() => onChangeStatus('')}
+                  style={is_etudiant ? RegisterStyle.pressed : RegisterStyle.button}
+                >
+                  <Text style={is_etudiant ? RegisterStyle.pressed_text : RegisterStyle.text}>
+                    Étudiant(e)
+                  </Text>
+                </Pressable>
+              </View>
+            </View>
+
+            <View style={[
+              RegisterStyle.container,
+              {
+                justifyContent: 'space-between',
+                alignItems: 'flex-start',
+                flex: 0.6,
+              }
+            ]}>
+              <View>
+                <Text style={[RegisterStyle.label]}>
+                  Nom de l'établissement
+                </Text>
+                <View style={[RegisterStyle.establishment,]}>
+                  <TextInput
+                    value={school.name}
+                    onChangeText={value => onChangeSchool('name', value)}
+                    style={{ color: "white", fontSize: 17 }}
+                    placeholderTextColor="rgba(20,20,20,0.3)"
+                    placeholder="Nom de l'établissement"
+                  ></TextInput>
+                </View>
+              </View>
+              <View>
+                <Text style={[RegisterStyle.label]}>
+                  Adresse de l'établissement
+                </Text>
+                <View style={RegisterStyle.establishment}>
+                  <TextInput
+                    value={school.address}
+                    onChangeText={value => onChangeSchool('address', value)}
+                    style={{ color: "white", fontSize: 17 }}
+                    placeholderTextColor="rgba(20,20,20,0.3)"
+                    placeholder="N° Rue, Rue, N° Département, Ville"
+                  ></TextInput>
+                </View>
+              </View>
+              <View>
+                <Text style={[RegisterStyle.label]}>
+                  Diplôme préparé
+                </Text>
+                <SelectDropdown
+                  buttonStyle={RegisterStyle.select}
+                  buttonTextStyle={{
+                    color: !school.diploma || school.diploma == diploma_types[0]
+                      ? 'rgba(20,20,20,0.3)' : 'white',
+                    fontSize: 17,
+                  }}
+                  dropdownStyle={{
+                    borderWidth: 0,
+                    borderRadius: 1,
+                  }}
+                  defaultButtonText="Choisir le diplôme"
+                  data={diploma_types}
+                  onSelect={(selectedDiploma, index) => {
+                    onChangeSchool('diploma', selectedDiploma != diploma_types[0] ? selectedDiploma : null);
+                  }}
+                  buttonTextAfterSelection={(selectedDiploma, index) => {
+                    return selectedDiploma;
+                  }}
+                  rowTextForSelection={(item, index) => {
+                    return item
+                  }}
+                />
+              </View>
+              <View>
+                <Text style={[RegisterStyle.label]}>
+                  Année d'obtention
+                </Text>
+                <SelectDropdown
+                  buttonStyle={RegisterStyle.select}
+                  buttonTextStyle={{
+                    color: !school.year_of_graduation || school.year_of_graduation == graduation_year[0]
+                      ? 'rgba(20,20,20,0.3)' : 'white',
+                    fontSize: 17,
+                  }}
+                  dropdownStyle={{
+                    borderWidth: 0,
+                    borderRadius: 1,
+                  }}
+                  defaultButtonText="Choisir l'année"
+                  data={graduation_year}
+                  onSelect={(selectedYear, index) => {
+                    onChangeSchool('year_of_graduation', selectedYear != graduation_year[0] ? selectedYear : null);
+                  }}
+                  buttonTextAfterSelection={(selectedYear, index) => {
+                    return selectedYear;
+                  }}
+                  rowTextForSelection={(item, index) => {
+                    return item
+                  }}
+                />
+              </View>
+            </View>
+
+            <View style={{
+              flex: 0.13
+            }}>
+            </View>
+          </View>
         </View>
-      </LinearGradient>
-    </View>
+      </LinearGradient >
+    </View >
   );
-  // const logoPath = require('../../assets/images/rte-logo.png');
-
-  // const [is_etudiant, set_is_etudiant] = useState(false);
-  // const [is_lyceen, set_is_lyceen] = useState(false);
-
-  // const select_type = (name: string) => {
-  //     if (name == 'lyceen') {
-  //         set_is_etudiant(false)
-  //         set_is_lyceen(!is_lyceen);
-  //     }
-  //     else {
-  //         set_is_lyceen(false);
-  //         set_is_etudiant(!is_etudiant);
-  //     }
-  // }
-
-  // const [diploma, setDiploma] = useState("none");
-
-  // return (
-  //     <View style={[MainContainer.container, RegisterStyle.container]}>
-  //         <View style={[RegisterStyle.main]}>
-  //             <Image source={logoPath} style={MainContainer.logo_in_the_app} />
-  //             <Text style={[RegisterStyle.text]}>
-  //                 Informations relatives à mon établissement :
-  //             </Text>
-  //             <Text style={[RegisterStyle.text]}>
-  //                 Je suis :
-  //             </Text>
-  //             <View style={{ borderWidth: 1, flexDirection: "row", justifyContent: "space-between", width: "90%" }}>
-  //                 <Pressable
-  //                     onPress={() => select_type('lyceen')}
-  //                     style={is_lyceen ? RegisterStyle.pressed : RegisterStyle.button}
-  //                 >
-  //                     <Text style={is_lyceen ? RegisterStyle.pressed_text : RegisterStyle.text}>
-  //                         Lycéen(ne)
-  //                     </Text>
-  //                 </Pressable>
-  //                 <Pressable
-  //                     onPress={() => select_type('')}
-  //                     style={is_etudiant ? RegisterStyle.pressed : RegisterStyle.button}
-  //                 >
-  //                     <Text style={is_etudiant ? RegisterStyle.pressed_text : RegisterStyle.text}>
-  //                         Étudiant(e)
-  //                     </Text>
-  //                 </Pressable>
-  //             </View>
-  //             <View style={{ borderWidth: 1, flexDirection: "column", justifyContent: "space-between", width: "90%" }}>
-  //                 <TextInput
-  //                     style={{ borderWidth: 1, color: "white" }}
-  //                     placeholder="Nom de l'établissement"
-  //                 ></TextInput>
-  //                 <TextInput
-  //                     style={{ borderWidth: 1, color: "white" }}
-  //                     placeholder="Adresse (complète) de l'établissement"
-  //                 ><Picker
-  //                     style={{ color: "white" }}
-  //                     selectedValue={diploma}
-  //                     onValueChange={(itemValue, itemIndex) =>
-  //                         setDiploma(itemValue)
-  //                     }>
-  //                         <Picker.Item label="None" value="None" />
-  //                         <Picker.Item label="Dut" value="Dut" />
-  //                         <Picker.Item label="Bts" value="Bts" />
-  //                         <Picker.Item label="Licence" value="License" style={{ color: "white" }} />
-  //                         <Picker.Item label="Master" value="Master" />
-  //                         <Picker.Item label="Doctora" value="Doctora" />
-  //                     </Picker>
-  //                 </TextInput>
-
-  //                 <Picker
-  //                     style={{ color: "white" }}
-  //                     selectedValue={diploma}
-  //                     onValueChange={(itemValue, itemIndex) =>
-  //                         setDiploma(itemValue)
-  //                     }>
-  //                     <Picker.Item label="None" value="None" />
-  //                     <Picker.Item label="Dut" value="Dut" />
-  //                     <Picker.Item label="Bts" value="Bts" />
-  //                     <Picker.Item label="Licence" value="License" style={{ color: "white" }} />
-  //                     <Picker.Item label="Master" value="Master" />
-  //                     <Picker.Item label="Doctora" value="Doctora" />
-  //                 </Picker>
-
-  //             </View>
-  //         </View>
-  //     </View>
-  // );
 }
 
-const styles = StyleSheet.create({
-  container: {
-
-    paddingTop: 10,
-    alignItems: "center",
-    borderWidth: 1,
-    width: '50%',
-  }
-});
 const RegisterStyle = StyleSheet.create({
   container: {
-    backgroundColor: '#90621C',
+
+    alignItems: 'center',
   },
   main: {
     flex: 1,
     flexDirection: 'column',
     alignItems: 'center',
-    width: '98%',
-    borderWidth: 1
+    justifyContent: 'space-between',
+
   },
   text: {
     color: 'white',
-    fontSize: 16,
+    fontSize: 17,
     fontWeight: 'bold',
-    letterSpacing: 0.25,
-    borderWidth: 1,
   },
 
   button: {
@@ -187,5 +271,31 @@ const RegisterStyle = StyleSheet.create({
     justifyContent: 'space-between',
     marginTop: 30,
   },
-
+  status_type: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    width: 350,
+    marginBottom: 40,
+  },
+  establishment: {
+    borderWidth: 1,
+    borderColor: 'white',
+    justifyContent: "center",
+    alignItems: 'center',
+    width: 350,
+    height: 50,
+  },
+  select: {
+    borderWidth: 1,
+    borderColor: 'white',
+    width: 350,
+    height: 50,
+    backgroundColor: null,
+  },
+  label: {
+    color: 'white',
+    fontSize: 18,
+    marginBottom: 10,
+  },
 })
